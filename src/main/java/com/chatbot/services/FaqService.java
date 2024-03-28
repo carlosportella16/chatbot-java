478c0696668e30ee4f3b6ea121d915d86f1ee943
carlosportella16 <carlosportella16@hotmail.com>
2024-03-28 18:45:32 -0300

    add string treatment before matching keyword

--- src/main/java/com/chatbot/services/FaqService.java
+++ src/main/java/com/chatbot/services/FaqService.java
@@ -5,16 +5,17 @@ import com.chatbot.utils.FaqAnswers;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class FaqService {
     final private FaqAnswers faqAnswers = new FaqAnswers();
 
     public String getAnswer(String question) {
-        List<String> words = List.of(question.toLowerCase().split("\\s+"));
+        List<String> words = Stream.of(question.toLowerCase().split("\\s+")).map(String::toLowerCase).toList();
         for (FaqAnswer entry : faqAnswers.getAnswers()) {
             for (String keyword : entry.getKeywords()) {
-                if (words.contains(keyword)) {
+                if (words.contains(keyword.toLowerCase())) {
                     return entry.getAnswer();
                 }
             }

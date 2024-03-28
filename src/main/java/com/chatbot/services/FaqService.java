16c01c090ad830314ba8eacc3ea0630b09047df9
carlosportella16 <carlosportella16@hotmail.com>
2024-03-28 19:21:55 -0300

    removing special characters and accentuation for better comparation

--- src/main/java/com/chatbot/services/FaqService.java
+++ src/main/java/com/chatbot/services/FaqService.java
@@ -3,6 +3,8 @@ package com.chatbot.services;
 import com.chatbot.domain.FaqAnswer;
 import com.chatbot.utils.FaqAnswers;
 import org.springframework.stereotype.Service;
+import java.text.Normalizer;
+
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,7 +14,12 @@ public class FaqService {
     final private FaqAnswers faqAnswers = new FaqAnswers();
 
     public String getAnswer(String question) {
-        List<String> words = Stream.of(question.toLowerCase().split("\\s+")).map(String::toLowerCase).toList();
+        // Remove a acentuacao e caracteres especiais
+        String questionFormated = removerAcentos(question);
+        // Separa as palavras da frase por espaço e coloca em lower case.
+        List<String> words = Stream.of(questionFormated.toLowerCase().split("\\s+")).map(String::toLowerCase).toList();
+        System.out.println(words);
+        // removendo acentuação gráfica das palavras
         for (FaqAnswer entry : faqAnswers.getAnswers()) {
             for (String keyword : entry.getKeywords()) {
                 if (words.contains(keyword.toLowerCase())) {
@@ -21,5 +28,14 @@ public class FaqService {
             }
         }
         return faqAnswers.getDefaultAnswer();
+    }
+
+    public String removerAcentos(String texto) {
+        // Utiliza a classe Normalizer para normalizar o texto com o Formato NFKD
+        // e então remove todos os caracteres que não são letras ou números
+        texto = Normalizer.normalize(texto, Normalizer.Form.NFKD);
+        texto = texto.replaceAll("[^\\p{ASCII}]", "")
+                .replaceAll("[^\\p{IsAlphabetic}\\s]", "");
+        return texto;
     }
 }
